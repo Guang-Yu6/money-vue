76cca99b6ed581999a5e6a3f8393b4d80c78c7e7
Guang-Yu6 <everyting-day@foxmail.com>
2021-06-11 00:15:02 +0800

    deploy

---
+++ precache-manifest.d2aa54473aa1fb4fd504aac07bbc26af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed9376f20067506da073",
+    "url": "/money-vue/css/app.27d29059.css"
+  },
+  {
+    "revision": "38c60f4104f1a9f32b2af6eeed15b4d3",
+    "url": "/money-vue/index.html"
+  },
+  {
+    "revision": "ed9376f20067506da073",
+    "url": "/money-vue/js/app.89e63b18.js"
+  },
+  {
+    "revision": "79427b2fea3e8f0b06a6",
+    "url": "/money-vue/js/chunk-vendors.757f8c8c.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/money-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-vue/robots.txt"
+  }
+]);
